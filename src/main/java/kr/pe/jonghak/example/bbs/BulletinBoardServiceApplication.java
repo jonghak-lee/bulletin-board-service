1c49c8cefd9412e42ad4fffa0312443b2bfc8799
이종학 <jonghak@ijonghag-ui-MacBookAir.local>
2019-12-06 13:13:45 +0900

    Initial commit

---
+++ src/main/java/kr/pe/jonghak/example/bbs/BulletinBoardServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.example.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinBoardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinBoardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pe/jonghak/example/bbs/BulletinBoardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.jonghak.example.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
