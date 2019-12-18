de34834b5db41b821006ce200c1db9f119d99b48
이종학 <jonghak@ijonghag-ui-MacBookAir.local>
2019-12-18 18:03:00 +0900

    Feign 클라이언트 활성화

--- src/main/java/kr/pe/jonghak/example/bbs/BulletinBoardServiceApplication.java
+++ src/main/java/kr/pe/jonghak/example/bbs/BulletinBoardServiceApplication.java
@@ -2,8 +2,10 @@ package kr.pe.jonghak.example.bbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BulletinBoardServiceApplication {
 
     public static void main(String[] args) {
